be8795b90af4baba4a2b882fd82657041c011d92
Mark Edmondson <github@markedmondson.me>
2016-11-09 16:14:31 +0100

    travis key on GCE for test-nodelete

--- tests/testthat/test_containers.R
+++ tests/testthat/test_containers.R
@@ -26,31 +26,31 @@ context("Google Container Registry")
 
 # 
 # ## this needs a container that can be saved quickly to avoid timeouts
-# test_that("Save docker containers", {
-#   skip_on_cran()
+test_that("Save docker containers", {
+  skip_on_cran()
+
+  vm <- gce_vm("test-container")
+
+  ## saves the running my-rstudio image that is named rstudio
+  ## commits and saves it to container registry as travis-test-container
+  cons <- harbor::containers(vm)
+  worked <- gce_save_container(vm,
+                               container_name = "travis-test-container",
+                               image_name = cons[[1]]$name,
+                               wait = TRUE
+                               )
+  expect_true(worked)
+})
 # 
-#   vm <- gce_vm("test-container")
 # 
-#   ## saves the running my-rstudio image that is named rstudio
-#   ## commits and saves it to container registry as travis-test-container
-#   cons <- harbor::containers(vm)
-#   worked <- gce_save_container(vm,
-#                                container_name = "travis-test-container",
-#                                image_name = cons[[1]]$name,
-#                                wait = TRUE
-#                                )
-#   expect_true(worked)
-# })
-# 
-# 
-# test_that("Load docker containers", {
-#   skip_on_cran()
-# 
-#   vm <- gce_vm("test-container")
-# 
-#   ## loads and runs an rstudio template from my projects container registry
-#   worked <- gce_load_container(vm,
-#                                container_name = "travis-test-container",
-#                                name = paste(sample(LETTERS, 15),collapse=""))
-#   expect_true(worked)
-# })
+test_that("Load docker containers", {
+  skip_on_cran()
+
+  vm <- gce_vm("test-container")
+
+  ## loads and runs an rstudio template from my projects container registry
+  worked <- gce_load_container(vm,
+                               container_name = "travis-test-container",
+                               name = paste(sample(LETTERS, 15),collapse=""))
+  expect_true(worked)
+})
