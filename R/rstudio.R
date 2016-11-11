8c769beeb25ae3059e051b7b9f151e020bd9c83a
Mark Edmondson <github@markedmondson.me>
2016-11-12 00:07:29 +0100

    adding users to rstudio (fix #20)

--- R/rstudio.R
+++ R/rstudio.R
@@ -5,26 +5,66 @@
 #' @param instance An instance with RStudio installed via \link{gce_vm_template}
 #' @param user The user to create
 #' @param password The user password
+#' @param container The rstudio container to add the user to
 #' 
 #' @return The instance
 #' @export
-gce_rstudio_adduser <- function(instance, user, password){
+gce_rstudio_adduser <- function(instance, user, password, 
+                                container = "rstudio",
+                                project = gce_get_global_project(),
+                                zone = gce_get_global_zone()){
   
-  # ssh_au <- paste0("adduser ",
-  #                   user, 
-  #                  " --gecos 'First Last,RoomNumber,WorkPhone,HomePhone' --disabled-password")
-  # 
-  # ssh_ap <- paste0("echo '",user,":",password,"' | chpasswd")
+  ssh_au <- paste0("adduser ",
+                    user,
+                   " --gecos 'First Last,RoomNumber,WorkPhone,HomePhone' --disabled-password")
   
-  #stop container and relaunch with new user
+  harbor::docker_cmd(instance,
+                     cmd = "exec",
+                     args = c(container, ssh_au),
+                     docker_opts = "")
   
-  # harbor::docker_cmd(instance, 
-  #                    cmd = "exec",
-  #                    args = c("rstudio", paste0(ssh_au, " && ", ssh_ap)),
-  #                    docker_opts = "")
+  harbor::docker_cmd(instance,
+                     "exec",
+                     args = c(container, "ls /home/"))
+  
+  ## add user folder to host VM
+
+  gce_rstudio_password(instance, 
+                       user = user, 
+                       password = password, 
+                       container = container, 
+                       project = project, 
+                       zone = zone)
   
   gce_set_metadata(list(rstudio_users = c(gce_get_metadata(instance, "rstudio_users")$value), user), 
                    instance)
+  
+  instance
+  
+}
+
+#' Changes password for a user on RStudio container
+#' 
+#' RStudio has users based on unix user accounts
+#' 
+#' @param instance An instance with RStudio installed via \link{gce_vm_template}
+#' @param user The user to chnage the password for
+#' @param password The user password
+#' @param container The rstudio container to add the user to
+#' 
+#' @return The instance
+#' @export
+gce_rstudio_password <- function(instance, user, password, 
+                                 container = "rstudio",
+                                 project = gce_get_global_project(),
+                                 zone = gce_get_global_zone()){
+  
+  ssh_ap <- paste0("sh -c 'echo ",user,":",password," | sudo chpasswd'")
+  
+  harbor::docker_cmd(instance,
+                     cmd = "exec",
+                     args = c(container, ssh_ap),
+                     docker_opts = "")
   
   instance
   
