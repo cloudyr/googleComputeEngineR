d460bba0ca4ee206b361674c90135482f04db340
Mark IIH <mark@iihnordic.com>
2017-10-10 14:59:37 +0200

    remove travis environment arg

--- R/options.R
+++ R/options.R
@@ -33,8 +33,7 @@
   }) 
   
   attempt <- try(googleAuthR::gar_attach_auto_auth("https://www.googleapis.com/auth/cloud-platform",
-                                    environment_var = "GCE_AUTH_FILE",
-                                    travis_environment_var = "TRAVIS_GCE_AUTH_FILE"))
+                                    environment_var = "GCE_AUTH_FILE"))
   if(inherits(attempt, "try-error")){
     warning("Tried to auto-authenticate but failed.")
   }
