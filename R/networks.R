9423f4da7bac682020b6c66bac0f857058d2ad5f
Jas Sohi <j450h1@gmail.com>
2018-02-18 00:38:08 -0800

    Implement and test utility function to make getting ac value less confusing

--- R/networks.R
+++ R/networks.R
@@ -38,22 +38,10 @@ gce_make_network <- function(name,
                              externalIP = NULL,
                              project = gce_get_global_project()){
   
-  net <- gce_get_network(network, project = project)
-  
-  if(!is.null(externalIP)){
-    if(externalIP == "none"){
-      ac <- NULL
-    } else { #ENTERED IP/VALID IF ALREADY CREATED 
-      ac <- list(
-        list(
-          natIP = jsonlite::unbox(externalIP),
-          type = jsonlite::unbox("ONE_TO_ONE_NAT"),
-          name = jsonlite::unbox(name)
-        )
-      )
-    }
-  } else { #NULL
-    ac <- list(
+  make_ac <- function(externalIP, name){
+    if(!is.null(externalIP) && externalIP == "none") return(NULL)
+    
+    list(
       list(
         natIP = jsonlite::unbox(externalIP),
         type = jsonlite::unbox("ONE_TO_ONE_NAT"),
@@ -62,11 +50,13 @@ gce_make_network <- function(name,
     )
   }
   
+  net <- gce_get_network(network, project = project)
+  
   structure(
     list(
       list(
         network = jsonlite::unbox(net$selfLink),
-        accessConfigs = ac
+        accessConfigs = make_ac(externalIP, name)
       )
     ),
     class = c("gce_networkInterface", "list")
--- tests/test_externalIP.R
+++ tests/test_externalIP.R
@@ -3,11 +3,12 @@ options(googleAuthR.scopes.selected = "https://www.googleapis.com/auth/cloud-pla
 devtools::load_all()
 
 vm <- gce_vm(template = "rstudio",
-             name = "rstudio-server-8",
+             name = "rstudio-server-10",
              username = "jas", password = "jas12345",
              predefined_type = "n1-standard-8",
+             #externalIP = "35.230.96.64" 
              #externalIP = "103.323.2323.232" #invalid ip
-             #externalIP = "35.230.96.64" #https://github.com/cloudyr/googleComputeEngineR/issues/69
              externalIP = "none"
+             #externalIP = NULL
 )
 
