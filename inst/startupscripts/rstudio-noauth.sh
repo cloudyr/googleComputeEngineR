c415514f6970e4fbcfd03e859c38fca9c8b6ad0c
John Muschelli <muschellij2@gmail.com>
2022-03-15 21:34:26 +0000

    added new features to mounting for docker

--- inst/startupscripts/rstudio-noauth.sh
+++ inst/startupscripts/rstudio-noauth.sh
@@ -11,10 +11,17 @@ GCER_DOCKER_IMAGE=$(curl http://metadata.google.internal/computeMetadata/v1/inst
 
 echo "Docker image: $GCER_DOCKER_IMAGE"
 
+if [ -f /home/gcer ];
+then
+  chmod 775 /home/gcer
+  vol_code="-v /home/gcer:/home/gcer"
+fi
 # as per https://www.rocker-project.org/use/managing_users/
 docker run -p 8787:8787 \
            -e ROOT=TRUE \
-           -e USER=rstudio -e DISABLE_AUTH=true \
+           -e USER=gcer \
+           ${vol_code} \
+           -e DISABLE_AUTH=true \
            --name=rstudio \
            --privileged=true \
            $GCER_DOCKER_IMAGE
